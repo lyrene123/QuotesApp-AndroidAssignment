c259606c53428a71fcc493612fd0d9bd24af0a06
Lyrene <laborlyrene@gmail.com>
2017-11-01 14:59:46 -0400

    Created the custom_category_list xml layout that will hold a category item in the list view. Added the authentication verification code in the main activity class, but partically done for now.

--- app/src/main/java/cs/dawson/myapplication/MainActivity.java
+++ app/src/main/java/cs/dawson/myapplication/MainActivity.java
@@ -1,13 +1,53 @@
 package cs.dawson.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String email = "user@droid.com";
+    private final String password = "iloveandroid";
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        //sign in into firebase to retrieve data
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                        } else {
+                            displayErrorAuthentication(task);
+                        }
+                    }
+                });
+
     }
+
+    private void displayErrorAuthentication(Task<AuthResult> task){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(task.getException().getMessage())
+                .setTitle(R.string.login_error_title)
+                .setPositiveButton(android.R.string.ok, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
---
+++ app/src/main/java/cs/dawson/myapplication/util/DBHelperUtil.java
@@ -0,0 +1,32 @@
+package cs.dawson.myapplication.util;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+
+/**
+ * Created by laborlyrene on 2017-11-01.
+ */
+
+public class DBHelperUtil {
+
+
+    private FirebaseAuth mFirebaseAuth;
+    //private FirebaseUser mFirebaseUser;
+
+    private DatabaseReference mDatabase;
+    private String mUserId;
+
+
+
+    public DBHelperUtil(){
+
+    }
+
+    public void signInToFirebase(){
+
+    }
+
+
+}
+
