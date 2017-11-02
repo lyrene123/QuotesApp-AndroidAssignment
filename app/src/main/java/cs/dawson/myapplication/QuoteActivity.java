4d32b53ea174be7486dafded3918421fa6213af0
Lyrene <laborlyrene@gmail.com>
2017-11-02 12:39:38 -0400

    Started coding the QuoteActivity, but still unsure how to proceed and how we can implement a bean class to fill up when retrieving a quote record from the database.

--- app/src/main/java/cs/dawson/myapplication/QuoteActivity.java
+++ app/src/main/java/cs/dawson/myapplication/QuoteActivity.java
@@ -10,9 +10,22 @@ import android.widget.TextView;
 
 public class QuoteActivity extends Activity {
 
+    TextView attributedTV, dateTV, birthdateTV, fullquoteTV, refTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
+
+        TextView quoteTitleTV = (TextView) findViewById(R.id.quoteTitle);
+        if ( getIntent().hasExtra("category_title") != false &&
+                getIntent().getExtras().getString("category_name") != null) {
+            quoteTitleTV.setText(quoteTitleTV.getText()
+                    + " " + getIntent().getExtras().getString("category_name"));
+        }
+    }
+
+    private void retrieveHandleToTextViews(){
+        //attributedTV = (TextView) findViewById(R.)
     }
 }
