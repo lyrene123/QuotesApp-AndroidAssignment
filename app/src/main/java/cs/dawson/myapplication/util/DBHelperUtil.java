a702e0496de78f4257cb4ea5294658901d12e888
Lyrene <laborlyrene@gmail.com>
2017-11-03 15:29:03 -0400

    Added more detailed comments in the DBHelperUtil class to make it easier to understand the methods and their function

--- app/src/main/java/cs/dawson/myapplication/util/DBHelperUtil.java
+++ app/src/main/java/cs/dawson/myapplication/util/DBHelperUtil.java
@@ -28,20 +28,18 @@ import cs.dawson.myapplication.model.QuoteItem;
 /**
  * Helper utility class that handles database access operations and authentication
  * for the firebase. This DAO class only handles read operations against the database.
- * Only when the authentication to the firebase is successful,
- * handles the retrieval of the names of the categories for the MainActivity
- * and loading them into a ListView with the help of a custom adapter.
- * Handles the retrieval as well of the short quotes from the firebase and loaded
- * into a ListView with a custom adapter for the QuoteListActivity. For the QuoteActivity,
- * handles the retrieval of all information of a particular quote of category and data
- * loaded into a model class.
- *
+ * Only when the authentication to the firebase is successful, handles the retrieval of
+ * the names of the categories for the MainActivity and loading them into a ListView
+ * with the help of a custom adapter. Handles the retrieval as well of the short quotes
+ * from the firebase and loaded into a ListView with a custom adapter for the QuoteListActivity.
+ * For the QuoteActivity, handles the retrieval of all information of a particular quote
+ * of category and data loaded into a model class.
  *
  * @author Lyrene Labor, Peter Bellefleur
  */
 public class DBHelperUtil {
 
-    //for the different list
+    //the different lists to fill with data from database
     private List<String> categoriesList;
     private List<String> quoteList;
     private CustomAdapter adapter;
@@ -54,6 +52,7 @@ public class DBHelperUtil {
     private final String email = "user@droid.com";
     private final String password = "iloveandroid";
 
+    //model class to fill up with quote information
     private  QuoteItem quote;
 
     /**
@@ -66,19 +65,20 @@ public class DBHelperUtil {
     }
 
     /**
-     * Retrieves the names of the categories or the short quotes of a category depending
-     * on the retrieve input String which indicates the type data we want to retrieve.
-     * But does database authentication first before retrieving any database records.
+     * Retrieves the names of the categories, the short quotes of a category, or all the info of a quote
+     * depending on the retrieve input String which indicates the type data we want to retrieve.
+     * But handles database authentication first before retrieving any database records.
      * If not, a dialog will display with an error message letting the user know
      * that an error has occurred
      *
-     * @param activity Activity that called this method (QuoteListActivity and QuoteActivity)
-     * @param list ListView that will contain the list of categories
-     *             (for MainActivity and QuoteListActivity)
-     * @param retrieve String containing the data you want to retrieve from the database
-     * @param categoryID the id of the category that was selected and which to display the short quotes
+     * @param activity Activity that needs access to the database
+     * @param list ListView that will contain the list of categories or short quotes
+     *             (applied for the MainActivity and QuoteListActivity)
+     * @param retrieve String containing the type of data you want to retrieve from the database
+     * @param categoryID the id of the category that was selected and used to retrieve the right
+     *                   the short quotes
      *                   (for QuoteListActivity and QuoteActivity)
-     * @param categoryTitle the name of the category that was selected and which to display short quotes
+     * @param categoryTitle the name of the category that was selected
      *                      (for QuoteListActivity and QuoteActivity)
      */
     public void retrieveRecordsFromDb(final Activity activity, final ListView list, final String retrieve,
@@ -90,7 +90,7 @@ public class DBHelperUtil {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            //if category names are the data you want to retrive
+                            //if category names are the data you want to retrieve
                             if(retrieve.equalsIgnoreCase("category")) {
                                 loadCategoriesFromDb(list, activity);
                             }
@@ -101,6 +101,7 @@ public class DBHelperUtil {
                                         categoryTitle);
                             }
 
+                            //if a quote and its related info are the data to retrieve
                             if(retrieve.equalsIgnoreCase("quote_item")){
                                 loadQuoteItemFromDb(categoryID, quoteID, activity);
                             }
@@ -120,9 +121,9 @@ public class DBHelperUtil {
      * the help of a CustomAdapter object.
      *
      * @param list ListView in which to load the short quotes from the database
-     * @param activity The activity that invoked the method
-     * @param categoryID the id of the category that was selected and which to display the short quotes
-     * @param categoryTitle the name of the category that was selected and which to display short quotes
+     * @param activity The activity that is requesting for the data
+     * @param categoryID the id of the category that was selected and used to display the short quotes
+     * @param categoryTitle the name of the category that was selected
      */
     private void loadCategoryShortQuoteFromDb(ListView list, Activity activity, int categoryID,
                                               String categoryTitle){
@@ -134,14 +135,16 @@ public class DBHelperUtil {
             /**
              * Retrieves any changes made to the database. In our case
              * this method will be called once at the beginning when
-             * retrieving the initial data.
+             * retrieving the initial data. Retrieves all the short quotes
+             * of a specific category and notifies the change to the adapter
              *
              * @param dataSnapshot DataSnapshot object data from db
              */
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot item : dataSnapshot.getChildren()){
-                    //add the retrieved data into the list of quotes
+
+                    //add the retrieved short quote into the list of quotes
                     addShortQuoteToList((String)item.child("quote_short").getValue());
                     Log.d("QUOTES-QLActivity", "Quote-short item is: " +
                             (String)item.child("quote_short").getValue());
@@ -161,8 +164,10 @@ public class DBHelperUtil {
             }
         };
 
+        //set the listener
         mDatabase.child("quotes").child(categoryID+"").addValueEventListener(listener);
 
+        //create the adapter and set it to the ListView to inflate the items
         adapter = new CustomAdapter(activity, quoteList, "QuoteListActivity", categoryID, categoryTitle);
         list.setAdapter(adapter);
     }
@@ -171,29 +176,32 @@ public class DBHelperUtil {
      * Retrieves the information of a quote of a particular category and loads
      * all data retrieved into a model class QuoteItem. Once all information has been
      * loaded into a QuoteItem instance, display the quote information in the view by
-     * calling the displayQuoteInfoInTextViews of the activity that called the method
+     * calling the displayQuoteInfoInTextViews of the activity that requested the data
      * which is the QuoteActivity.
      *
-     * @param categoryID the id of the category in which the quote to retrieve belongs to
+     * @param categoryID the id of the category in used the quote to retrieve belongs to
      * @param quoteID the id of the quote to be retrieved
-     * @param activity Activity that called this method which is the QuoteActivity
+     * @param activity Activity that requested the data which is the QuoteActivity
      */
     private void loadQuoteItemFromDb(int categoryID, int quoteID, final Activity activity){
 
-        //create the ValueEventListener listener object
+        //create the ValueEventListener object
         ValueEventListener listener = new ValueEventListener() {
             /**
              * Retrieves any changes made to the database. In our case
              * this method will be called once at the beginning when
-             * retrieving the initial data.
+             * retrieving the initial data. Retrieves and load the quote
+             * into a QuoteItem instance
              *
              * @param dataSnapshot DataSnapshot object data from db
              */
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                //retrieve and load the quote into a QuoteItem instance
                 setQuoteItem(dataSnapshot.getValue(QuoteItem.class));
                 Log.d("QUOTES-QuoteList", "Retrieved quote item: " + quote.getDate_added());
 
+                //once quote is loaded into a QuoteItem instance, display the quote info
                 if(quote != null){
                     ((QuoteActivity) activity).displayQuoteInfoInTextViews(quote);
                 }
@@ -217,10 +225,10 @@ public class DBHelperUtil {
 
     /**
      * Retrieves the list of categories from the database and instantiates the custom
-     * adapter in order to set the data retrieved into the ListView
+     * adapter in order to load the category names into the ListView
      *
      * Solution based on :
-     * solution based on https://stackoverflow.com/questions/41434475/how-to-list-data-from-firebase-database-in-listview
+     * https://stackoverflow.com/questions/41434475/how-to-list-data-from-firebase-database-in-listview
      *
      * @param list ListView object which we want to load the data into
      * @param activity Activity that called the method
@@ -234,13 +242,15 @@ public class DBHelperUtil {
             /**
              * Retrieves any changes made to the database. In our case
              * this method will be called once at the beginning when
-             * retrieving the initial data.
+             * retrieving the initial data. Retrieves the name of
+             * the categories and notifies the adapter
              *
              * @param dataSnapshot DataSnapshot object data from db
              */
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot item : dataSnapshot.getChildren()){
+
                     //add the retrieved data into the list of categories
                     addCategoryToList((String)item.child("name").getValue());
                     Log.d("QUOTES-MainActivity", "Category item is: " +
@@ -287,8 +297,7 @@ public class DBHelperUtil {
     }
 
     /**
-     * Adds a category string into the list of Strings that holds the names of
-     * categories
+     * Adds a category string name into the list of categories
      *
      * @param category category name to add to the list
      */
